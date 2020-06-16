6a83b71d27ec6dedc51636cd792ba1ceabf09189
Diego Veiga Santos <sdiegoveiga@gmail.com>
2020-06-16 14:13:11 -0300

    feat: TDD tests in existings services

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,40 @@
+import AppError from '@shared/errors/AppError';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '12345',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('12345');
+  });
+
+  it('should not be able to create two appointments in the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointmentDate = new Date(2020, 5, 16, 15);
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '12345',
+    });
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '12345',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
