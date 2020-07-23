3c367631404e6091fb094f311d2bdf90ffb83a15
Diego Veiga Santos <sdiegoveiga@gmail.com>
2020-07-23 14:41:15 -0300

    finished back-end

---
+++ src/config/mail.ts
@@ -0,0 +1,20 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: process.env.MAIL_CONFIG_EMAIL,
+      name: process.env.MAIL_CONFIG_NAME,
+    },
+  },
+} as IMailConfig;
