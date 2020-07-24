cdbf7af7606afd8977c225a160d85f91f3469011
Diego Veiga Santos <sdiegoveiga@gmail.com>
2020-07-24 10:58:57 -0300

    fix data to front-end

--- src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -2,11 +2,12 @@ import { Response, Request } from 'express';
 import { container } from 'tsyringe';
 
 import ListProviderAppointmentsService from '@modules/appointments/services/ListProviderAppointmentsService';
+import { classToClass } from 'class-transformer';
 
 export default class ProviderAppointmentsControler {
   public async index(request: Request, response: Response): Promise<Response> {
     const provider_id = request.user.id;
-    const { day, month, year } = request.body;
+    const { day, month, year } = request.query;
 
     const listProviderAppointments = container.resolve(
       ListProviderAppointmentsService,
@@ -14,11 +15,11 @@ export default class ProviderAppointmentsControler {
 
     const appointment = await listProviderAppointments.execute({
       provider_id,
-      day,
-      month,
-      year,
+      day: Number(day),
+      month: Number(month),
+      year: Number(year),
     });
 
-    return response.json(appointment);
+    return response.json(classToClass(appointment));
   }
 }
